e9ae4aec08def6e23836b7695abe6bf5a50eb02b
GibsonCS <gibson.cruzds@gmail.com>
2025-04-16 20:46:15 -0300

    feat: configs fastify cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,13 @@
 import { fastify, FastifyInstance } from "fastify";
 import { registerRoutes } from "./routes/index.js";
+import fastifyCors from "@fastify/cors";
 
 const server: FastifyInstance = fastify();
+server.register(fastifyCors, {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+});
+
 registerRoutes(server);
 
 const startServer = async (): Promise<void> => {
